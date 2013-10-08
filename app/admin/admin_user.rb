2fdf32f4caa6bb59296052d043ad3887a8899dc8
frossia <frossiacsb@gmail.com>
2013-10-08 15:00:40 +0400

    AdminUser patch

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -21,4 +21,12 @@ ActiveAdmin.register AdminUser do
     end
     f.actions
   end
+
+  controller do
+    #...
+    def permitted_params
+      params.permit(:admin_user => [:email, :password, :password_confirmation])
+    end
+  end
+
 end
