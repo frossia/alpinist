d20d959ae18657c177cc7a0d5005e3e319384e08
frossia <frossiacsb@gmail.com>
2013-12-06 02:11:09 +0400

    uff

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,1 +1,1 @@
-ActiveAdmin.register_page "Dashboard" do  menu :priority => 1, :label => "Главная"end
+ActiveAdmin.register_page "Dashboard" do  menu :priority => 1, :label => "Главная"end
